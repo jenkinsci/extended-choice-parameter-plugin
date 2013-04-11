2996e349c6d44da9fad427b068b93311a73dc589
Mike Chmielewski <mikec@bullhorn.com>
2013-04-11 09:53:26 -0400

    Fixed a typo.

--- src/main/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterDefinition.java
+++ src/main/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterDefinition.java
@@ -139,7 +139,7 @@ public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 		}
 		this.visibleItemCount = visibleItemCount;
 		
-		if (multiSelectDelimter.equals("")) {
+		if(multiSelectDelimiter.equals("")) {
 			multiSelectDelimiter = ",";
 		}
 		this.multiSelectDelimiter = multiSelectDelimiter;
