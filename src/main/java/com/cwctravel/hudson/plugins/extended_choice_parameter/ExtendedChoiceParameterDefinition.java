a00bfcbd2b11c0556d09f592cade3dd82a010e34
John DiMatteo <jdimatteo@gmail.com>
2013-09-13 08:59:57 -0400

    Multi level multi select now implemented

--- src/main/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterDefinition.java
+++ src/main/java/com/cwctravel/hudson/plugins/extended_choice_parameter/ExtendedChoiceParameterDefinition.java
@@ -6,6 +6,7 @@ import hudson.model.ParameterDefinition;
 import hudson.util.FormValidation;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -14,6 +15,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedHashMap;
 
 import javax.servlet.ServletException;
 
@@ -27,6 +31,8 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
+import au.com.bytecode.opencsv.CSVReader;
+
 public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 	private static final long serialVersionUID = -2946187268529865645L;
 
@@ -39,6 +45,10 @@ public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 	public static final String PARAMETER_TYPE_RADIO = "PT_RADIO";
 
 	public static final String PARAMETER_TYPE_TEXT_BOX = "PT_TEXTBOX";
+        
+	public static final String PARAMETER_TYPE_MULTI_LEVEL_SINGLE_SELECT = "PT_MULTI_LEVEL_SINGLE_SELECT";
+        
+	public static final String PARAMETER_TYPE_MULTI_LEVEL_MULTI_SELECT = "PT_MULTI_LEVEL_MULTI_SELECT";
 
 	@Extension
 	public static class DescriptorImpl extends ParameterDescriptor {
@@ -106,7 +116,7 @@ public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 	private String type;
 
 	private String value;
-
+		
 	private String propertyFile;
 
 	private String propertyKey;
@@ -189,9 +199,9 @@ public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 		}
 		return null;
 	}
-
+	
 	@Override
-	public ParameterValue createValue(StaplerRequest request, JSONObject jO) {
+	public ParameterValue createValue(StaplerRequest request, JSONObject jO) {		
 		Object value = jO.get("value");
 		String strValue = "";
 		if(value instanceof String) {
@@ -199,7 +209,34 @@ public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 		}
 		else if(value instanceof JSONArray) {
 			JSONArray jsonValues = (JSONArray)value;
-			strValue = StringUtils.join(jsonValues.iterator(), getMultiSelectDelimiter());
+			if (type.equals(PARAMETER_TYPE_MULTI_LEVEL_MULTI_SELECT))
+			{
+				final int valuesBetweenLevels = this.value.split(",").length;
+				
+				Iterator it = jsonValues.iterator();
+				for (int i = 0; it.hasNext(); i++)
+				{
+					if (i != 0)
+					{
+						if (i % valuesBetweenLevels == 0)
+						{
+							// todo: insert a new line here
+							//       unfortunately StringParameterValue doesn't support newlines,
+							//       but TextParameterValue does
+							strValue += "__END_OF_MULILEVEL_SELECTION__";
+						}
+						else
+						{
+							strValue += ",";
+						}
+					}
+					strValue += it.next();
+				}
+			}
+			else
+			{
+				strValue = StringUtils.join(jsonValues.iterator(), getMultiSelectDelimiter());
+			}
 		}
 
 		if(quoteValue) {
@@ -219,7 +256,8 @@ public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 		}
 		return super.getDefaultParameterValue();
 	}
-
+	
+	// note that computeValue is not called by multiLevel.jelly
 	private String computeValue(String value, String propertyFilePath, String propertyKey) {
 		if(!StringUtils.isBlank(propertyFile) && !StringUtils.isBlank(propertyKey)) {
 			try {
@@ -290,7 +328,156 @@ public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 	public String getEffectiveValue() {
 		return computeValue(value, propertyFile, propertyKey);
 	}
+	
+	private ArrayList<Integer> columnIndicesForDropDowns(String[] headerColumns)
+	{
+		ArrayList<Integer> columnIndicesForDropDowns = new ArrayList<Integer>();
+		
+		String[] dropDownNames = value.split(",");
 
+		for (String dropDownName : dropDownNames)
+		{
+			for (int i = 0; i < headerColumns.length; ++i)
+			{
+				if (headerColumns[i].equals(dropDownName))
+				{
+					columnIndicesForDropDowns.add(new Integer(i));
+				}
+			}
+		}
+		
+		return columnIndicesForDropDowns;
+	}
+	
+	LinkedHashMap<String, LinkedHashSet<String>> calculateChoicesByDropdownId() throws Exception
+	{
+		List<String[]> fileLines =
+			new CSVReader(new FileReader(propertyFile), '\t').readAll();
+
+		if (fileLines.size() < 2)
+		{
+			throw new Exception("Multi level tab delimited file must have at least 2 "
+							+ "lines (one for the header, and one or more for the data)");
+		}
+
+		ArrayList<Integer> columnIndicesForDropDowns =
+						columnIndicesForDropDowns(fileLines.get(0));
+		
+		List<String[]> dataLines = fileLines.subList(1, fileLines.size());
+
+		LinkedHashMap<String, LinkedHashSet<String>> choicesByDropdownId =
+						new LinkedHashMap<String, LinkedHashSet<String>>();
+
+		String prefix = getName() + " dropdown MultiLevelMultiSelect 0";
+		choicesByDropdownId.put(prefix, new LinkedHashSet<String>());
+
+		for (int i=0; i < columnIndicesForDropDowns.size(); ++i)
+		{
+			String prettyCurrentColumnName = value.split(",")[i];
+			prettyCurrentColumnName = prettyCurrentColumnName.toLowerCase();
+			prettyCurrentColumnName = prettyCurrentColumnName.replace("_", " ");
+
+			for (String[] dataLine : dataLines)
+			{
+				String priorLevelDropdownId = prefix;
+				String currentLevelDropdownId = prefix;
+
+				int column = 0;
+				for (int j=0; j <= i; ++j)
+				{
+					column = columnIndicesForDropDowns.get(j);
+
+					if (j < i)
+					{
+						priorLevelDropdownId += " " + dataLine[column];
+					}
+					currentLevelDropdownId += " " + dataLine[column];
+				}					
+				if (i != columnIndicesForDropDowns.size() - 1)
+				{
+					choicesByDropdownId.put(currentLevelDropdownId, new LinkedHashSet<String>());
+				}
+				LinkedHashSet<String> choicesForPriorDropdown
+								= choicesByDropdownId.get(priorLevelDropdownId);
+				choicesForPriorDropdown.add("Select a " + prettyCurrentColumnName
+																		+ "...");
+				choicesForPriorDropdown.add(dataLine[column]);
+			}				
+		}
+
+		return choicesByDropdownId;
+	}
+	
+	public String getMultiLevelDropdownIds() throws Exception
+	{
+		String dropdownIds = new String();
+		
+		LinkedHashMap<String, LinkedHashSet<String>> choicesByDropdownId = 
+						calculateChoicesByDropdownId();
+		
+		for (String id : choicesByDropdownId.keySet())
+		{
+			if (dropdownIds.length() > 0)
+			{
+				dropdownIds += ",";
+			}
+			dropdownIds += id;
+		}
+				
+		return dropdownIds;
+		
+		/* dropdownIds is of a form like this:
+		return name + " dropdown MultiLevelMultiSelect 0," 
+				   // next select the source of the genome -- each genome gets a seperate dropdown id:"
+				 + name + " dropdown MultiLevelMultiSelect 0 HG18,dropdown MultiLevelMultiSelect 0 ZZ23,"
+				 // next select the cell type of the source -- each source gets a seperate dropdown id
+				 + name + " dropdown MultiLevelMultiSelect 0 HG18 Diffuse large B-cell lymphoma, dropdown MultiLevelMultiSelect 0 HG18 Multiple Myeloma,"
+				 + name + " dropdown MultiLevelMultiSelect 0 ZZ23 Neuroblastoma,"
+				 // next select the name from the cell type -- each cell type gets a seperate dropdown id
+				 + name + " dropdown MultiLevelMultiSelect 0 HG18 Diffuse large B-cell lymphoma LY1,"
+				 + name + " dropdown MultiLevelMultiSelect 0 HG18 Multiple Myeloma MM1S,"
+				 + name + " dropdown MultiLevelMultiSelect 0 ZZ23 Neuroblastoma BE2C,"
+				 + name + " dropdown MultiLevelMultiSelect 0 ZZ23 Neuroblastoma SKNAS";*/
+	}
+	
+	public Map<String, String> getChoicesByDropdownId() throws Exception
+	{
+		LinkedHashMap<String, LinkedHashSet<String>> choicesByDropdownId = 
+			calculateChoicesByDropdownId();
+		
+		Map<String, String> collapsedMap = new LinkedHashMap<String, String>();
+		
+		for (String dropdownId : choicesByDropdownId.keySet())
+		{
+			String choices = new String();
+			for (String choice : choicesByDropdownId.get(dropdownId))
+			{
+				if (choices.length() > 0)
+				{
+					choices += ",";
+				}
+				choices += choice;
+			}
+			
+			collapsedMap.put(dropdownId, choices);
+		}
+				
+		/* collapsedMap is of a form like this:
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0", "Select a genome...,HG18,ZZ23");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 HG18", "Select a source...,Diffuse large B-cell lymphoma,Multiple Myeloma");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 ZZ23", "Select a source...,Neuroblastoma");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 HG18 Diffuse large B-cell lymphoma","Select a cell type...,LY1");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 HG18 Multiple Myeloma","Select a cell type...,MM1S");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 ZZ23 Neuroblastoma","Select a cell type...,BE2C,SKNAS");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 HG18 Diffuse large B-cell lymphoma LY1","Select a name...,LY1_BCL6_DMSO,LY1_BCL6_JQ1");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 HG18 Multiple Myeloma MM1S", "Select a name...,MM1S_BRD4_150nM_JQ1,MM1S_BRD4_500nM_JQ1");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 ZZ23 Neuroblastoma BE2C", "Select a name...,BE2C_BRD4");
+		collapsedMap.put(name + " dropdown MultiLevelMultiSelect 0 ZZ23 Neuroblastoma SKNAS", "Select a name...,SKNAS_H3K4ME3");
+		*/
+		
+		return collapsedMap;
+	}
+	
 	public String getValue() {
 		return value;
 	}
@@ -342,5 +529,4 @@ public class ExtendedChoiceParameterDefinition extends ParameterDefinition {
 	public Map<String, Boolean> getDefaultValueMap() {
 		return computeDefaultValueMap();
 	}
-
 }
